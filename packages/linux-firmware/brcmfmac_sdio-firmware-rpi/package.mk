04c96883862c8195e7c3fee984cd52a83de87b86
Tomáš Kelemen (vudiq) <vudiq@vudiq.sk>
2024-04-30 22:34:18 +0200

    brcmfmac_sdio-firmware-rpi: udpate from devel

--- packages/linux-firmware/brcmfmac_sdio-firmware-rpi/package.mk
+++ packages/linux-firmware/brcmfmac_sdio-firmware-rpi/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="brcmfmac_sdio-firmware-rpi"
-PKG_VERSION="ea9963f3f77b4bb6cd280577eb115152bdd67e8d"
-PKG_SHA256="e51b717c2a60ca29fcdd8e04e07c00996226cb48fa56a8ad1934b5f4ddee2e3d"
+PKG_VERSION="26ff205b45dc109b498a70aaf182804ad9dbfea5"
+PKG_SHA256="2f0917b104739455dd488dd8f5af2ee4430801a7ac8fe8d9866e74bfbb185356"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/LibreELEC/LibreELEC.tv"
 PKG_URL="https://github.com/LibreELEC/${PKG_NAME}/archive/${PKG_VERSION}.tar.gz"
@@ -14,15 +14,4 @@ PKG_TOOLCHAIN="manual"
 
 makeinstall_target() {
   DESTDIR=${INSTALL}/$(get_kernel_overlay_dir) ./install
-}
-
-post_makeinstall_target() {
-  # Install rpi btuart script to bring up Bluetooth
-  mkdir -p ${INSTALL}/usr/bin
-    cp -P ${PKG_DIR}/scripts/rpi-btuart ${INSTALL}/usr/bin
-    cp -P ${PKG_DIR}/scripts/rpi-udev ${INSTALL}/usr/bin
-}
-
-post_install() {
-  enable_service brcmfmac_sdio-firmware.service
 }
