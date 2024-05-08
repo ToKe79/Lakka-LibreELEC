c5d1f68fd0521841d7de7c1a45d26fed7dad9524
Tomáš Kelemen (vudiq) <vudiq@vudiq.sk>
2024-05-08 23:01:37 +0200

    fluidsynth: build dynamic library, remove patch
    Lakka uses dynamic libsndfile
    dynamic fluidsynth needed for easyrpg

--- packages/audio/fluidsynth/package.mk
+++ packages/audio/fluidsynth/package.mk
@@ -12,7 +12,7 @@ PKG_DEPENDS_TARGET="toolchain glib libsndfile"
 PKG_LONGDESC="FluidSynth renders midi music files as raw audio data, for playing or conversion."
 PKG_BUILD_FLAGS="+pic"
 
-PKG_CMAKE_OPTS_TARGET="-DBUILD_SHARED_LIBS=0 \
+PKG_CMAKE_OPTS_TARGET="-DBUILD_SHARED_LIBS=1 \
                        -DLIB_SUFFIX= \
                        -Denable-libsndfile=1 \
                        -Denable-pkgconfig=1 \
